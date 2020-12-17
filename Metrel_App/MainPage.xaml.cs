88bb2710f123b0d2496feeb2251b9ad03c17e813
Nejc Pivec <nejc.pivec1994@gmail.com>
2020-12-17 17:04:11 +0100

    Initial commit

---
+++ Metrel_App/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Metrel_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Decode_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Decode button was clicked");
+        }
+
+
+        private void Callback_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Callback button was clicked");
+        }
+    }
+}
